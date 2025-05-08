7fba26a452c207fce6ff8d83f4c19a119a16d167
lanbery <cwd-pc@xy-info.com>
2025-05-08 17:04:48 +0800

    chore(): release v0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# @exaify/yml-config
+
+## 0.0.1
+
+### Patch Changes
+
+- Feat: first version
--- scripts/push-changelog.mjs
+++ scripts/push-changelog.mjs
@@ -1,11 +1,15 @@
 import { execSync } from 'child_process';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 
 async function main() {
-  const { default: pkg } = await import('../package.json', {
-    assert: {
-      type: 'json',
-    },
-  });
+  // const { default: pkg } = await import('../package.json', {
+  //   assert: {
+  //     type: 'json',
+  //   },
+  // });
+  const pkg = await require('../package.json');
+  console.log(pkg);
 
   const commandAdd = `git add .`;
   const commandCommit = `git commit -am "chore(): release v${pkg.version}"`;
