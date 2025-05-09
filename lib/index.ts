cea9a10a3e7a8cd47e4cead47f1dc3d8c7780eee
lanbery <cwd-pc@xy-info.com>
2025-05-09 11:36:21 +0800

    feat: add expose constants

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 export * from './interfaces';
 export * from './loaders';
-
+export * from './config.constants';
 export * from './conditional.module';
 export * from './config.module';
 export * from './config.service';
